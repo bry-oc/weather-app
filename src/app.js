385b5c29d5aedc5ab5f06333537f2d58424e8149
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-01-26 21:43:34 -0500

    templates setup

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
-const publicStaticDirPath = path.join(__dirname, '../public')
+const publicStaticDirPath = path.join(__dirname, '../public');
 
+const viewsPath = path.join(__dirname, '../templates/views');
 
+const partialPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
 app.use(express.static(publicStaticDirPath));
 
 app.get('', (req,res) => {
